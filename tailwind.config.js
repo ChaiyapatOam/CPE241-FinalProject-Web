0b1f9348b1745dbfd76fe31791082023904427ab
Chaiyapat Oam <chaiyapat.meey@mail.kmutt.ac.th>
2024-05-01 22:46:58 +0700

    style: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-primary": "#0B85FF",
+        accent: "#87BCFF",
+        "dark-gray": "#404040",
+        secondary: "#FFE738",
+      },
+    },
   },
   plugins: [],
 };
